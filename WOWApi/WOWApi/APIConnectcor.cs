b21d7e7f9f4cca2aca607eb20cb5e6f86744de78
jayras <jayras@gmail.com>
2012-12-26 18:25:14 -0800

    Signed-off-by: jayras <jayras@gmail.com>

---
+++ WOWApi/WOWApi/APIConnectcor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace WOWApi
+{
+    class APIConnectcor
+    {
+        class APIConnector
+        {
+            public string Region { get; set; }
+            public string BaseURI
+            {
+                get { return GetURIFromRegion(Region); }
+            }
+
+            public APIConnector()
+            {
+
+            }
+
+
+            public string GetResponse(string uri)
+            {
+                HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
+
+                if (request == null)
+                {
+                    throw new ArgumentException("Invalid URI");
+                }
+
+                request.Method = "GET";
+                request.ContentType = "application/json";
+
+                string result = null;
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+                catch (WebException e)
+                {
+                    HttpWebResponse response = e.Response as HttpWebResponse;
+                    WebExceptionStatus s = e.Status;
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+                    throw new WOWAPIException((int)response.StatusCode, result, e);
+
+                }
+
+                return result;
+            }
+
+            private string GetURIFromRegion(string region)
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+    }
+}
---
+++ WOWApi/WOWApi/WOWAPIException.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace WOWApi
+{
+    public class WOWAPIError
+    {
+        [JsonIgnore]
+        public int Code { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        public override string ToString()
+        {
+
+            return Code.ToString() + ": " + Reason;
+        }
+    }
+
+    class WOWAPIException : Exception
+    {
+        public WOWAPIError Error { get; private set; }
+        public string RawResult { get; private set; }
+        public override string Message
+        {
+            get { return Error.Code.ToString() + ": " + Error.Reason; }
+        }
+
+        public WOWAPIException() : base() { }
+        public WOWAPIException(string message)
+            : base(message)
+        {
+            if (message.Contains("\"status\":\"nok\""))
+            {
+                Error = ParseResult(0, message);
+            }
+        }
+
+        public WOWAPIException(int code, string result, Exception inner)
+            : base(result, inner)
+        {
+            RawResult = result;
+            Error = ParseResult(code, result);
+        }
+
+        public WOWAPIException(int code, string result)
+        {
+            RawResult = result;
+            Error = ParseResult(code, result);
+        }
+
+        private static WOWAPIError ParseResult(int code, string result)
+        {
+            WOWAPIError error = JsonConvert.DeserializeObject<WOWAPIError>(result);
+            error.Code = code;
+            return error;
+        }
+
+    }
+}
